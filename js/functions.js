729f01a5a7c47500de42f9f77feddf8f4a13f1e6
Jan Kowalski <fatality1922@gmail.com>
2020-11-10 16:55:50 +0100

    add gra z funkcjami

--- js/functions.js
+++ js/functions.js
@@ -7,3 +7,39 @@ function printMessage(msg){
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
+
+function getMoveName(argMoveId){
+	if(argMoveId == 1){
+	  return 'kamień';
+	} 
+		else if(argMoveId == 2){
+			return 'papier';
+		}
+		else if (argMoveId ==3){
+			return 'nożyce';
+		}
+		else {
+			printMessage('Wprowadz prawidłową wartość');
+			return 'nieznany ruch';
+		}
+	}
+  
+  
+  function displayResult(ComputerMove, PlayerMove){
+	printMessage('Zagrałem ' + ComputerMove + ', a Ty ' + PlayerMove);
+  
+	if(( computerMove == 'kamień' && playerMove == 'papier')||
+    (computerMove == 'nożyce' && playerMove == 'kamień')||
+    (computerMove == 'papier' && playerMove == 'nożyce')){
+    printMessage('Ty wygrywasz!');
+  	}	
+		else if (computerMove == playerMove){
+				printMessage('Remis');
+		}
+		else if (playerMove == 'nieznany ruch'){
+				printMessage('Wprowadź prawidłową wartość');
+		}
+			else printMessage ('Nie popisałeś się, komputer wygrywa');
+	}
+
+  
