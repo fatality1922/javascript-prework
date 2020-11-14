6e269305a1645a12374b8c6511e96cbea4e95961
Jan Kowalski <fatality1922@gmail.com>
2020-11-14 14:10:41 +0100

    scripts variables edit

--- js/functions.js
+++ js/functions.js
@@ -1,45 +1,48 @@
-function printMessage(msg){
+function printMessage(msg) {
 	let div = document.createElement('div');
 	div.innerHTML = msg;
 	document.getElementById('messages').appendChild(div);
 }
 
-function clearMessages(){
+function clearMessages() {
 	document.getElementById('messages').innerHTML = '';
 }
 
-function getMoveName(argMoveId){
-	if(argMoveId == 1){
-	  return 'kamień';
-	} 
-		else if(argMoveId == 2){
-			return 'papier';
-		}
-		else if (argMoveId ==3){
-			return 'nożyce';
-		}
-		else {
-			printMessage('Wprowadz prawidłową wartość');
-			return 'nieznany ruch';
-		}
+function getMoveName(argMoveId) {
+	if (argMoveId == 1) {
+		return 'kamień';
 	}
-  
-  
-  function displayResult(computerMove, playerMove){
-	printMessage('Zagrałem ' + computerMove + ', a Ty ' + playerMove);
-  
-	if(( computerMove == 'kamień' && playerMove == 'papier')||
-    (computerMove == 'nożyce' && playerMove == 'kamień')||
-    (computerMove == 'papier' && playerMove == 'nożyce')){
-    printMessage('Ty wygrywasz!');
-  	}	
-		else if (computerMove == playerMove){
-				printMessage('Remis');
-		}
-		else if (playerMove == 'nieznany ruch'){
-				printMessage('Wprowadź prawidłową wartość');
-		}
-			else printMessage ('Nie popisałeś się, komputer wygrywa');
+	else if (argMoveId == 2) {
+		return 'papier';
 	}
+	else if (argMoveId == 3) {
+		return 'nożyce';
+	}
+	else {
+		printMessage('Wprowadz prawidłową wartość');
+		return 'nieznany ruch';
+	}
+}
 
-  
+
+function displayResult(computerMove, playerMove) {
+	printMessage('Zagrałem ' + computerMove + ', a Ty ' + playerMove);
+
+	if ((computerMove == 'kamień' && playerMove == 'papier') ||
+		(computerMove == 'nożyce' && playerMove == 'kamień') ||
+		(computerMove == 'papier' && playerMove == 'nożyce')) {
+		printMessage('Ty wygrywasz!');
+		playerScore = playerScore + 1;
+	}
+	else if (computerMove == playerMove) {
+		printMessage('Remis');
+	}
+	else if (playerMove == 'nieznany ruch') {
+		printMessage('Wprowadź prawidłową wartość');
+	}
+	else {
+		printMessage('Nie popisałeś się, komputer wygrywa');
+		computerScore = computerScore + 1;
+	}
+}
+
--- js/script.js
+++ js/script.js
@@ -1,27 +1,56 @@
-function playGame(playerInput){
-  clearMessages()
+let playerScore = 0;
+let computerScore = 0;
 
+function playGame(playerInput) {
+  clearMessages();
 
+  // if (playerScore == 10 || computerScore == 10) {
+  //   printMessage('koniec gry');
+  //   return;
+  // }
 
-  let randomNumber = Math.floor(Math.random() * 3 + 1);
-  console.log('Wylosowana liczba to: ' + randomNumber);
-  let computerMove = getMoveName(randomNumber);
-  // let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
   console.log('Gracz wpisał: ' + playerInput);
-  let playerMove = getMoveName(playerInput);
+  const playerMove = getMoveName(playerInput);
+
+  if (Math.random() > 0.75) {
+    if (playerMove == 'papier') {
+      computerMove = 'nożyce';
+    } else if (playerMove == 'kamień') {
+      computerMove = 'papier';
+    } else {
+      computerMove = 'kamień';
+    }
+
+  } else {
+    if (playerMove == 'papier') {
+      computerMove = 'kamień';
+    } else if (playerMove == 'kamień') {
+      computerMove = 'nożyce';
+    } else {
+      computerMove = 'papier';
+    }
+  }
+
+  // let randomNumber = Math.floor(Math.random() * 3 + 1);
+  // console.log('Wylosowana liczba to: ' + randomNumber);
+  // let computerMove = getMoveName(randomNumber);
 
   displayResult(computerMove, playerMove);
-
+  printMessage('Gracz ' + playerScore + ' Komputer ' + computerScore);
 
 }
+while (playerScore < 1000) {
+  playGame(1);
+}
 
-  document.getElementById('kamień').addEventListener('click', function(){
-    playGame(1);
-  });
-  document.getElementById('papier').addEventListener('click', function(){
-    playGame(2);
-  });
-  document.getElementById('nożyce').addEventListener('click', function(){
-    playGame(3);
-  });
+document.getElementById('kamień').addEventListener('click', function () {
+  playGame(1);
+});
+document.getElementById('papier').addEventListener('click', function () {
+  playGame(2);
+});
+document.getElementById('nożyce').addEventListener('click', function () {
+  playGame(3);
+});
+
 
